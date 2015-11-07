046dc6c0a7c4660e053781eac72b766f32c6379f
Megan Ramp <mer7@mac023373.local>
2015-11-06 19:13:47 -0500

    final push of assign 4

---
+++ contact.php
@@ -0,0 +1,157 @@
+<!doctype html>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Bryson's BBQ | Welcome</title>
+    <link rel="stylesheet" href="stylesheets/foundation.css" />
+    <link rel="stylesheet" href="stylesheets/foundation-icons.css" />
+    <link rel="stylesheet" href="stylesheets/style.css" />
+    <link rel="stylesheet" href="stylesheets/app.css" />
+    <link rel="stylesheet" href="stylesheets/lightbox.css">
+    <link rel="stylesheet" href="scss/_custom.scss">
+    <link rel="stylesheet" type="text/css" href="slick/slick.css"/>
+  <link rel="stylesheet" type="text/css" href="slick/slick-theme.css"/>
+      
+  </head>
+     <style>
+  	
+            
+    body {
+    background: url("images/brick.jpg");
+    
+}
+	.slider{
+	  width: 800px;
+	  height: 400px;
+	  margin: 20px auto;	
+	}
+  </style>
+  <body>
+         
+<nav class="top-bar" data-topbar role="navigation">
+  <ul class="title-area">
+    <li class="name">
+      <h1><a href="#">236 Yazoo Ave,
+Clarksdale, MS. 3861</a></h1>
+    </li>
+  </ul>
+
+  <section class="top-bar-section">
+    <!-- Right Nav Section -->
+    <ul class="right">
+       <li><a href="http://bwm399.mramp413.com/assign-04/index.html">Home</a></li>
+        <li><a href="http://bwm399.mramp413.com/assign-04/menu.html">Menu</a></li>
+        <li><a href="http://bwm399.mramp413.com/assign-04/photo.html">Photos</a></li>
+        <li><a href="http://bwm399.mramp413.com/assign-04/contact.php">Contact</a></li>
+    </ul>
+  </section>
+</nav>
+      
+        
+<header>
+    <div class="slider">
+     <div><img src="img/slider1.jpg"></div>
+      <div><img src="img/slider2.jpg"></div>
+      <div><img src="img/slider3.jpg"></div>
+  </div>
+    
+  <div class="row boxes">
+         <div class="large-12 columns">
+            <h2>Contact Us</h2>
+        </div>
+      </div>
+</header>
+      
+      
+    
+      <Section>
+          <div class="row boxes">
+            <div class="large-12 columns">
+              <div class="panel">
+  <div class="col-xs-12 col-sm-5 col-md-7">
+      <form class="form-horizontal" role="form" method="post" action="contact.php">
+          <div class="form-group">
+            <label for="name" class="col-sm-2 control-label"><div class='black'>Name</div></label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" name="name" id="name"
+                     
+                     value=""   
+                     
+                     > 
+                              
+            </div>
+            </div>
+          <div class="form-group">
+            <label for="email" class="col-sm-2 control-label"><div class='black'>Email</div></label>
+            <div class="col-sm-10">
+              <input type="email" class="form-control" name="email" id="email" 
+                     value="   ">
+                
+                            </div>
+          </div>
+          <div class="form-group">
+			<label for="message" class="col-sm-2 control-label"><div class='black'>Message</div></label>
+			<div class="col-sm-10">
+			<textarea class="form-control" name="message" rows="4" placeholder="Enter message here..."></textarea>
+						</div>
+			</div>
+          <div class="form-group">
+            <label for="human" class="col-sm-2 control-label" value="   "><div class='black'>2 + 3 = ?</div></label>
+            <div class="col-sm-10">
+              <input type="text" class="form-control" name="human" id="human" placeholder="Your Answer" value="   ">
+                            </div>
+          </div>
+          <div class="form-group">
+            <div class="col-sm-offset-2 col-sm-10">
+              <div class="center"><button type="input" name="submit" class="btn btn-default"><div class='black'>Send</div></button></div>
+            </div>
+          </div>
+          <div class="form-group">    
+            <div class="col-sm-offset-2 col-sm-10">
+               <!-- Will be used to display an alert to the user -->  
+                            </div>
+            
+        </form>
+              
+   
+</Section>
+          
+          <div class="footer">
+        <div class="large-4 columns">
+            <h4>Contact Us</h4>
+            <address>236 Yazoo Ave, <br>
+Clarksdale, MS. 3861<br>
+                <a href="mailto: mer7@pct.edu">Brysonsbbq@gmail.com</a><br />
+            (662) 902-5508<br>
+            </address>
+            mer7@pct.edu<br>
+        </div>
+        <div class="large-4 columns">
+            <h4>What We're About</h4>
+            <p>In 2013 our dream came true and we opened up Bryson’s BBQ in Clarksdale Mississippi. We have a full bar that features 20 micro beers on tap daily.We serve lunch and dinner. On Sundays features the BBQ, Blues, and Brunch with gospel in from 9-noon and blues from noon-3. The history in the era and all the great people here seemed like the perfect place to open up. Clarksdale is known by many to be the home of Delta blues, blues bars, and numerous festivals with gives people all the reason to visit. </p>
+        </div>
+        <div class="large-4 columns">
+            <h4>Find Us</h4>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m24!1m12!1m3!1d6599.582573532834!2d-90.57352022892945!3d34.202808744872186!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!4m9!1i0!3e0!4m0!4m5!1s0x862adf0f52374b95%3A0xb0bdb2a41df30498!2s236+Yazoo+Ave%2C+Clarksdale%2C+MS+38614!3m2!1d34.20209!2d-90.5735095!5e0!3m2!1sen!2sus!4v1415755153387" width="270" height="170" frameborder="0" style="border:0"></iframe>
+        </div>   
+          </div>
+          
+          
+    <script src="bower_components/jquery/dist/jquery.min.js"></script>
+    <script src="bower_components/foundation/js/foundation.min.js"></script>
+    <script src="js/app.js"></script>
+    
+      
+      <script type="text/javascript" src="http://code.jquery.com/jquery-1.11.0.min.js"></script>
+      <script type="text/javascript" src="http://code.jquery.com/jquery-migrate-1.2.1.min.js"></script>
+      <script type="text/javascript" src="slick/slick.min.js"></script>
+
+  <script type="text/javascript">
+    $(document).ready(function(){
+      $('.slider ').slick( );
+    });
+  </script>
+  
+  </body>
+</html>
